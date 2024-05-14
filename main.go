7669b3813a55b0ba149b82d9390a11bb92fc5514
Justas Kazakauskas <justas.kazakauskas@gmail.com>
2024-05-14 10:29:36 +0300

    Add better logging

--- main.go
+++ main.go
@@ -11,32 +11,44 @@ import (
 
 func main() {
     http.HandleFunc("/", redisPingHandler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+
+    addr := ":8080"
+
+    log.Printf("Listening on %s...\n", addr)
+    log.Fatal(http.ListenAndServe(addr, nil))
 }
 
 func redisPingHandler(w http.ResponseWriter, r *http.Request) {
     redisHost := os.Getenv("REDIS_HOST")
     if redisHost == "" {
-        http.Error(w, "REDIS_HOST environment variable not set", http.StatusInternalServerError)
+        errMsg := "REDIS_HOST environment variable not set"
+        http.Error(w, errMsg, http.StatusInternalServerError)
+        log.Println(errMsg)
         return
     }
 
     conn, err := redis.Dial("tcp", redisHost+":6379")
     if err != nil {
-        http.Error(w, "Failed to connect to Redis", http.StatusInternalServerError)
+        errMsg := fmt.Sprintf("Failed to connect to Redis: %v", err)
+        http.Error(w, errMsg, http.StatusInternalServerError)
+        log.Println(errMsg)
         return
     }
     defer conn.Close()
 
     pong, err := redis.String(conn.Do("PING"))
     if err != nil {
-        http.Error(w, "Redis PING failed", http.StatusInternalServerError)
+        errMsg := fmt.Sprintf("Redis PING failed: %v", err)
+        http.Error(w, errMsg, http.StatusInternalServerError)
+        log.Println(errMsg)
         return
     }
 
     if pong == "PONG" {
         fmt.Fprintln(w, "Redis connection successful!")
     } else {
-        http.Error(w, "Unexpected response from Redis", http.StatusInternalServerError)
+        errMsg := fmt.Sprintf("Unexpected response from Redis: %s", pong)
+        http.Error(w, errMsg, http.StatusInternalServerError)
+        log.Println(errMsg)
     }
 }
